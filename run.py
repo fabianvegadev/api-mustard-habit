83b94f51265f9cae4e2782263ee19184c398b209
Fabian Velasquez <fabianvegadev@gmail.com>
2024-10-19 01:51:19 -0500

    Ajuste de requirements.txt

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from app import create_app
+import os
 
 # Crear la aplicación Flask usando la función factory `create_app`
 app = create_app()
@@ -6,4 +7,5 @@ app = create_app()
 # Punto de entrada principal para ejecutar la aplicación
 if __name__ == '__main__':
     # Ejecutar la aplicación Flask con el modo debug activado
-    app.run(debug=True)
+    # Escuchar en todas las interfaces y utilizar el puerto especificado por la variable de entorno
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
