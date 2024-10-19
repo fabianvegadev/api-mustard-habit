2d9f534940eed6187c3291cfab552e3fd0d0c9fc
Fabian Velasquez <fabianvegadev@gmail.com>
2024-10-19 13:57:50 -0500

    Ajuste para inicializar la base de datos

--- start_server.py
+++ start_server.py
@@ -1,34 +1,7 @@
 import subprocess
 import os
 
-def run_commands():
-    # Comando para iniciar migraciones de base de datos
-    try:
-        print("Iniciando versiones de base de datos...")
-        subprocess.run(['flask', 'db', 'init'], check=True)
-        print("Migraciones iniciadas con éxito.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error al iniciar migraciones: {e}")
-        return
-
-    # Comando para iniciar migraciones de base de datos
-    try:
-        print("Iniciando migraciones de base de datos...")
-        subprocess.run(['flask', 'db', 'migrate', '-m', '"Initial migration."'], check=True)
-        print("Migraciones iniciadas con éxito.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error al iniciar migraciones: {e}")
-        return
-
-    # Comando para aplicar migraciones de base de datos
-    try:
-        print("Aplicando migraciones de base de datos...")
-        subprocess.run(['flask', 'db', 'upgrade'], check=True)
-        print("Migraciones aplicadas con éxito.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error al aplicar migraciones: {e}")
-        return
-    
+def run_commands():    
     # Comando para iniciar el servidor con Gunicorn
     try:
         print("Iniciando el servidor con Gunicorn...")
